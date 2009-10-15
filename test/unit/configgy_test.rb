0be30009707b897098d43c49b756045114b694ee
Robey Pointer <robey@twitter.com>
2009-10-14 17:27:41 -0700

    evan showed me that ruby can do unicode.

--- test/unit/configgy_test.rb
+++ test/unit/configgy_test.rb
@@ -21,8 +21,7 @@ class ConfiggyParserTest < Test::Unit::TestCase
   def test_unquote_c
     assert_equal "nothing", unquoted('"nothing"')
     assert_equal "name\tvalue\tDb\xfcllet?", unquoted('"name\\tvalue\\t\\x44b\\xfcllet?"')
-    # ruby can't do unicode.
-#    assert_equal "name\tvalue\t\342\202\254b\303\274llet?\342\202\254", unquoted("name\\tvalue\\t\\u20acb\\xfcllet?\\u20ac")
+    assert_equal "name\tvalue\t\342\202\254b\303\274llet?\342\202\254", unquoted('"name\\tvalue\\t\\u20acb\\u00fcllet?\\u20ac"')
     assert_equal "she said \"hello\"", unquoted('"she said \\"hello\\""')
     assert_equal "\\backslash", unquoted('"\\\\backslash"')
     assert_equal "real\\$dollar", unquoted('"real\\$dollar"')
