63944777cbb71758c853f0e1d9727579ac84fde9
Robey Pointer <robey@twitter.com>
2009-10-19 11:40:42 -0700

    add a global method Configgy.load_file for convenience.

--- lib/configgy.rb
+++ lib/configgy.rb
@@ -5,3 +5,9 @@ require "configgy/config_map"
 require "configgy/config_parser"
 
 class ConfigException < RuntimeError; end
+
+module Configgy
+  def self.load_file(filename)
+    Configgy::ConfigParser.new.load_file(filename)
+  end
+end
